5110f8e6f4c2d2fe2febecc20bcad9a72570823a
Arunsh93 <Arunhosur93@gmail.com>
2021-11-01 12:22:43 +0530

    Edit existing Contact

--- AddressBookUsingMultiThread/AddressBookDetails.cs
+++ AddressBookUsingMultiThread/AddressBookDetails.cs
@@ -56,6 +56,105 @@ namespace AddressBookUsingMultiThread
             Console.WriteLine("Successfully Added!");
         }
 
+        public static void EditContact()
+        {
+            int f;
+            if (contacts.Count > 0)
+            {
+                Console.WriteLine("Name of Person you want to Edit: ");
+                string editName = Console.ReadLine();
+
+                foreach (var name in contacts)
+                {
+                    if (editName.ToLower() == name.firstName.ToLower())
+                    {
+                        while (true)
+                        {
+                            f = 0;
+                            Console.WriteLine("1.First name\n2.Last name\n3.Address\n4.City\n5.State\n6.ZipCode\n7.Phone Number\n8.email\n9.Exit");
+                            Console.WriteLine("Enter Option You want to edit");
+                            int option = int.Parse(Console.ReadLine());
+                            switch (option)
+                            {
+                                case 1:
+                                    Console.Write("Edit First name: ");
+                                    name.firstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.Write("Edit Last name: ");
+                                    name.lastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.Write("Edit Address: ");
+                                    name.address = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.Write("Edit City name: ");
+                                    name.city = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.Write("Edit State: ");
+                                    name.state = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.Write("Edit Zipcode: ");
+                                    name.zip = int.Parse(Console.ReadLine());
+                                    break;
+                                case 7:
+                                    while (true)
+                                    {
+                                        Console.WriteLine("Enter Phone Number: ");
+                                        string phoneNumber = Console.ReadLine();
+                                        if (phoneNumber.Length == 10)
+                                        {
+                                            name.phoneNumber = phoneNumber;
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Phone Number Invalid! Phone number should have 10 digits!");
+                                        }
+                                    }
+                                    break;
+                                case 8:
+                                    while (true)
+                                    {
+                                        Console.WriteLine("Enter EmailId: ");
+                                        string email = Console.ReadLine();
+                                        if (email.Contains("@"))
+                                        {
+                                            name.emailId = email;
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Emailid Should Contain @ ");
+                                        }
+                                    }
+                                    break;
+                                case 9:
+                                    Console.WriteLine("Edited!");
+                                    f = 1;
+                                    break;
+                            }
+                            if (f == 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entered Name is Not in Contact!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Yours Contact List is Empty!");
+            }
+        }
+
         public static void ViewConatcts()
         {
             if (contacts.Count > 0)
--- AddressBookUsingMultiThread/Program.cs
+++ AddressBookUsingMultiThread/Program.cs
@@ -10,19 +10,27 @@ namespace AddressBookUsingMultiThread
 
             while (true)
             {
-                Console.WriteLine("1. Add Member to Contact List \n2. View Members in Contact list \n3.Exit");
-                Console.Write("Enter an Option: ");
-                int option = int.Parse(Console.ReadLine());
-                switch (option)
+                Console.WriteLine("****Welcome to Address Book Using Collection****");
+
+                while (true)
                 {
-                    case 1:
-                        AddressBookDetails.AddContact();
-                        break;
-                    case 2:
-                        AddressBookDetails.ViewConatcts();
-                        break;
-                    case 3:
-                        return;
+                    Console.WriteLine("1. Add Member to Contact List \n2. View Members in Contact list \n3. Edit Contact List \n4.Exit");
+                    Console.Write("Enter an Option: ");
+                    int option = int.Parse(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            AddressBookDetails.AddContact();
+                            break;
+                        case 2:
+                            AddressBookDetails.ViewConatcts();
+                            break;
+                        case 3:
+                            AddressBookDetails.EditContact();
+                            break;
+                        case 4:
+                            return;
+                    }
                 }
             }
         }
